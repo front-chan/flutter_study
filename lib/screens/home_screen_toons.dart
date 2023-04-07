906bcfba105af677cdd803f412cbacb515547277
front-chan <chan.jor.zz@gmail.com>
2023-04-08 01:48:31 +0900

    ListView study

--- lib/screens/home_screen_toons.dart
+++ lib/screens/home_screen_toons.dart
@@ -5,7 +5,7 @@ import 'package:flutter_study/services/api_service.dart';
 class HomeScreenToons extends StatelessWidget {
   HomeScreenToons({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +24,49 @@ class HomeScreenToons extends StatelessWidget {
           future: webtoons,
           builder: ((context, snapshot) {
             if (snapshot.hasData) {
-              return const Text('There is data!');
+              // Text 출력
+              // return const Text('There is data!');
+
+              // ListView 전체 타이틀 출력
+              // return ListView(
+              //   children: [
+              //     for (var webtoon in snapshot.data!)
+              //       Text(webtoon.title,
+              //           style: const TextStyle(
+              //             fontSize: 20,
+              //           )),
+              //   ],
+              // );
+
+              // ListView.builder
+              // return ListView.builder(
+              //   // 수직/수평 스크롤 기능
+              //   scrollDirection: Axis.vertical,
+              //   itemCount: snapshot.data!.length,
+              //   itemBuilder: (context, index) {
+              //     var webtoon = snapshot.data![index];
+              //     return Text(webtoon.title);
+              //   },
+              // );
+
+              // ListView.separated
+              return ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  // print(index);
+                  var webtoon = snapshot.data![index];
+                  return Text(webtoon.title);
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 20,
+                ),
+              );
             }
-            return const Text('Loading...');
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+            // return const Text('Loading...');
           }),
         ));
   }
