3a11befa48d72d343887c6f9a5f2ec53c16bff01
front-chan <chan.jor.zz@gmail.com>
2023-04-07 21:31:31 +0900

    fromjson study

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter_study/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      // print(response.body);
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        // print(webtoon);
+        // final toon = WebtoonModel.fromJson(webtoon);
+        // print(toon.title);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
